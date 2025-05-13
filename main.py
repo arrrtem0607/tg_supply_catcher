47e9655958a3976c2cb8ad7da713ae44eac4b3aa
naillora <naillora@student.21-school.ru>
2025-05-13 21:22:03 +0300

    Выделил инициализацию вебхука в отдельный сервис

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import asyncio
-import uvicorn
 
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
 from aiogram.client.default import DefaultBotProperties
@@ -11,44 +8,10 @@ from aiogram_dialog import setup_dialogs
 
 from configurations import get_config
 from bot import get_all_routers
-from services.payments.service import router as webhook_router
 from services.utils.logger import setup_logger
 from bot.middlewares.db_middleware import initialize_database
-from services.payments.client import TochkaAPIClient
-from configurations.payments_config import PaymentsConfig
 
 logger = setup_logger(__name__)
-
-# -- Lifespan для FastAPI --
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await asyncio.sleep(5)  # ⏱️ дать FastAPI полностью подняться
-    client = TochkaAPIClient(PaymentsConfig())
-    try:
-        url = "https://82ef-45-144-233-139.ngrok-free.app/webhook/tochka/"  # Заменить на актуальный
-        await client.setup_webhook(url)
-        logger.info("✅ Вебхук Точки успешно зарегистрирован")
-    except Exception as e:
-        logger.error("❌ Ошибка при регистрации вебхука Точки: %s", e)
-
-    yield  # ⏳ Запуск приложения
-
-    try:
-        await client.delete_webhook()
-        logger.info("🗑️ Вебхук Точки удалён при завершении работы")
-    except Exception as e:
-        logger.error("❌ Не удалось удалить вебхук Точки: %s", e)
-
-# -- FastAPI app --
-app = FastAPI(lifespan=lifespan)
-app.include_router(webhook_router)
-
-
-async def run_fastapi():
-    config = uvicorn.Config(app, host="0.0.0.0", port=8010, log_level="info")
-    server = uvicorn.Server(config)
-    await server.serve()
-
 
 async def setup_bot_commands(bot: Bot):
     commands = [
@@ -60,8 +23,7 @@ async def setup_bot_commands(bot: Bot):
     ]
     await bot.set_my_commands(commands)
 
-
-async def run_bot():
+async def main():
     config = get_config()
 
     redis = Redis(host="localhost")
@@ -83,14 +45,6 @@ async def run_bot():
     logger.info("✅ Бот успешно запущен")
 
     await dp.start_polling(bot)
-
-
-async def main():
-    await asyncio.gather(
-        run_fastapi(),
-        run_bot(),
-    )
-
 
 if __name__ == "__main__":
     asyncio.run(main())
